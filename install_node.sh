ca44499f357a5f4f49184fbb93231a2eb0c5d20f
Wael BEN ZID ELGUEBSI <benzid.wael@hotmail.fr>
2014-11-24 14:21:32 +0100

    Improve code & add a global argument.

--- install_node.sh
+++ install_node.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+
+OPTS=`getopt -o g --long node-version,global -n 'parse-options' -- "$@"`
+
+eval set -- "$OPTS"
+
+# FIXME Maybe it's better to install only nvm if the node version is not
+# specified.
+# default node version
+NODE_VERSION=0.10.33
+GLOBAL=false
+
+### Start getopts code ###
+while [ $# -gt 0 ]; do
+  case $1 in
+    -g | --global)
+      GLOBAL=true
+      ;;
+    --node-version) NODE_VERSION=true ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not recognized."
+      echo -e "Use ${BOLD}$SCRIPT -h${NORM} to see the help documentation."\\n
+      exit 2
+      ;;
+  esac
+  shift
+done
+### End getopts code ###
+
 cd /tmp
 
 # Clean
@@ -9,12 +37,16 @@ wget https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash
 
 source ~/.profile
 
-# Install Node 0.10.33
-nvm install 0.10.33
+# Install node
+nvm install $NODE_VERSION
 
 cat << EOF >> ~/.profile
 
-nvm use 0.10.33
+nvm use $NODE_VERSION
 EOF
+
+if [ "$GLOBAL" == true ]; then
+  n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+fi
 
 source ~/.profile
